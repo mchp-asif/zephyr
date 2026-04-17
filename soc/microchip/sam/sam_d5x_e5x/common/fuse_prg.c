d5bf78488a4f71b314cee7f7e871655eb7fad7a8
Muhammed Asif <muhammed.asif@microchip.com>
2026-04-17 19:15:40 +0530

    added the soc files for fusebit setting
    - this commit contains changes for getting the usernode from device tree
      and creates a linker section from it using a newly added ld script
    - then it will place the data of fuse bit in that location
    
    Signed-off-by: Muhammed Asif <muhammed.asif@microchip.com>

---
+++ soc/microchip/sam/sam_d5x_e5x/common/fuse_prg.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2026 Microchip Technology Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/devicetree.h>
+#include <zephyr/init.h>
+#include <zephyr/sys/util.h>
+#include <soc.h>
+
+__attribute__((section(".userpage")))const fuses_user_fuses_registers_t user_nvm_data = {0,0xdeadbeef,0xdadadada};
+//  = {0, 0, 0, 0, 0xff, 0xff, 0x24, 0x95}
+// const unsigned char user_nvm_data[256] = {0, 0, 0, 0, 0x11, 0x00, 0xdc, 0x1d};
+// const unsigned char user_nvm_data[256] = {0x0, 0x0, 0x0, 0x0, 0xDE, 0xAD, 0xBE, 0xEF};
